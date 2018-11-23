0e6a3dc69425b2104237e2af5d6fa9efb34587ad
wei.huang <wei.huang@rakuten.de>
2018-11-23 22:02:04 +0100

    PoC for chacha20

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,38 +1,42 @@
-use ring::aead::chacha20_poly1305_openssh as chacha;
-// use ring::aead::SealingKey;
 use ring::aead::*;
 use ring::rand::{SecureRandom, SystemRandom};
 use std::io::{Read, Result, Write};
 
-type Token = [u8; 64];
-type Nonce = [u8; 16];
+type Token = [u8; 32];
+type Nonce = [u8; 12];
 
-pub struct Encoder {
+pub struct Encryption {
     token: Token,
     nonce: Nonce,
     sealing_key: SealingKey,
 }
-impl Encoder {
+impl Encryption {
     pub fn new() -> Self {
         let token = gen_token();
-        Encoder {
+        Encryption {
             token,
-            nonce: [0u8; 16],
+            nonce: gen_nonce(),
             sealing_key: SealingKey::new(&CHACHA20_POLY1305, &token).unwrap(),
         }
     }
 
     pub fn with_token(token: &Token) -> Self {
-        Encoder {
+        Encryption {
             token: *token,
-            nonce: [0u8; 16],
+            nonce: gen_nonce(),
             sealing_key: SealingKey::new(&CHACHA20_POLY1305, token).unwrap(),
         }
     }
-}
 
-impl Read for Encoder {
-    fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
+    pub fn token(&self) -> Token {
+        self.token
+    }
+
+    pub fn nonce(&self) -> Nonce {
+        self.nonce
+    }
+
+    pub fn encrypt(&mut self, buf: &mut [u8]) -> Result<usize> {
         Ok(seal_in_place(
             &self.sealing_key,
             &self.nonce,
@@ -44,34 +48,32 @@ impl Read for Encoder {
     }
 }
 
-impl Write for Encoder {
-    fn write(&mut self, buf: &[u8]) -> Result<usize> {
-        unimplemented!()
-    }
-    fn flush(&mut self) -> Result<()> {
-        unimplemented!()
-    }
+pub struct Decryption {
+    nonce: Nonce,
+    opening_key: OpeningKey,
 }
 
-pub struct Decoder {}
-
-impl Read for Decoder {
-    fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
-        unimplemented!()
+impl Decryption {
+    pub fn new(token: &Token, nonce: &Nonce) -> Self {
+        Decryption {
+            nonce: *nonce,
+            opening_key: OpeningKey::new(&CHACHA20_POLY1305, token).unwrap(),
+        }
     }
-}
 
-impl Write for Decoder {
-    fn write(&mut self, buf: &[u8]) -> Result<usize> {
-        unimplemented!()
-    }
-    fn flush(&mut self) -> Result<()> {
-        unimplemented!()
+    pub fn decrypt(&mut self, buf: &mut [u8]) -> Vec<u8> {
+        Vec::from(open_in_place(&self.opening_key, &self.nonce, &[], 0, buf).unwrap())
     }
 }
 
 fn gen_token() -> Token {
-    let mut token = [0u8; 64];
+    let mut token = [0u8; 32];
     SystemRandom::new().fill(&mut token).unwrap();
     token
+}
+
+fn gen_nonce() -> Nonce {
+    let mut nonce = [0u8; 12];
+    SystemRandom::new().fill(&mut nonce).unwrap();
+    nonce
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,29 @@
+#![feature(toowned_clone_into)]
 extern crate ring;
-extern crate clap;
 
 mod crypto;
 mod file;
 
-fn main() {
+use ring::aead::*;
 
+fn main() {
+    let token = [1u8; 32];
+    let mut en = crypto::Encryption::with_token(&token);
+    let mut de = crypto::Decryption::new(&token, &en.nonce());
+
+    // let mut buf: Vec<u8> = Vec::new();
+    // String::from("Af").as_bytes().clone_into(&mut buf);
+    let content = b"content to encrypt".to_vec();
+    // Ring uses the same input variable as output
+    let mut in_out = content.clone();
+
+    // The input/output variable need some space for a suffix
+    println!("Tag len {}", CHACHA20_POLY1305.tag_len());
+    for _ in 0..CHACHA20_POLY1305.tag_len() {
+        in_out.push(0);
+    }
+    println!("0: {:?}", in_out);
+    en.encrypt(&mut in_out).unwrap();
+    println!("a: {:?}", in_out);
+    println!("b: {:?}", de.decrypt(&mut in_out));
 }
