d17adf2d9fe32c09ff6b0d7927a49171f79918b3
wei.huang <william.hng@outlook.com>
2018-11-26 14:33:53 +0100

    Playing with mmap (Yeah~!

---
+++ src/file.rs
@@ -0,0 +1,71 @@
+use memmap::{Mmap, MmapMut, MmapOptions};
+use std::fs::{self, File, OpenOptions};
+use std::io::Result;
+
+pub struct FileReader<'a> {
+    file: &'a File,
+    file_size: usize,
+    cursor: usize,
+    chunk_size: usize,
+}
+
+impl<'a> FileReader<'a> {
+    pub fn new(file: &File, chunk_size: usize) -> FileReader {
+        FileReader {
+            file,
+            file_size: file.metadata().unwrap().len() as usize,
+            chunk_size,
+            cursor: 0,
+        }
+    }
+
+    pub fn next_mmap(&mut self) -> Option<Mmap> {
+        if self.cursor >= self.file_size {
+            return None;
+        }
+        let mut mmap_option = MmapOptions::new();
+        mmap_option
+            .offset(self.cursor as u64)
+            .len(std::cmp::min(self.chunk_size, self.file_size - self.cursor));
+
+        self.cursor += self.chunk_size;
+        Some(unsafe { mmap_option.map(&self.file) }.unwrap())
+    }
+}
+
+pub struct FileWriter<'a> {
+    file: &'a File,
+    file_size: usize,
+    cursor: usize,
+    chunk_size: usize,
+}
+
+impl<'a> FileWriter<'a> {
+    pub fn new(file: &File, file_size: usize, chunk_size: usize) -> FileWriter {
+        file.set_len(file_size as u64).unwrap();
+        FileWriter {
+            file,
+            file_size,
+            chunk_size,
+            cursor: 0,
+        }
+    }
+
+    pub fn next_mmap(&mut self) -> Option<MmapMut> {
+        if self.cursor >= self.file_size {
+            return None;
+        }
+        let mut mmap_option = MmapOptions::new();
+        mmap_option
+            .offset(self.cursor as u64)
+            .len(std::cmp::min(self.chunk_size, self.file_size - self.cursor));
+
+        self.cursor += self.chunk_size;
+        Some(unsafe { mmap_option.map_mut(&self.file) }.unwrap())
+    }
+}
+
+
+const CYPHER_FILE_HEADER_LEN: usize = 8; // The size of the original file
+const PLAIN_CHUNK_LEN: usize = 0x6400000; // 100Mb
+const CYPHER_CHUNK_LEN: usize = PLAIN_CHUNK_LEN + 16; // 100Mb + tag_len
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,30 @@
 extern crate base64;
 extern crate ring;
 mod crypto;
+mod file;
+use std::fs::{self, File, OpenOptions};
 
 use self::crypto::*;
+use self::file::*;
 
 fn main() {
-    let key = Key::from("MTIzNDU2Nzg5MDEyMzQ1Njc4OTAxMjM0NTY3ODkwMDA=");
-    let mut en = Encryption::new(key);
-    let mut de = Decryption::new(key);
+    // let key = Key::from("MTIzNDU2Nzg5MDEyMzQ1Njc4OTAxMjM0NTY3ODkwMDA=");
+    // let mut en = Encryption::new(key);
+    // let mut de = Decryption::new(key);
 
-    let mut in_out = b"d".to_vec();
-    in_out.extend_from_slice(&vec![0u8; 16]);
+    // let mut in_out = b"d".to_vec();
+    // in_out.extend_from_slice(&vec![0u8; 16]);
 
-    println!("0: {:?}", in_out);
-    en.encrypt(&mut in_out);
-    println!("a: {:?}", in_out);
-    de.decrypt(&mut in_out);
-    println!("b: {:?}", in_out);
+    // println!("0: {:?}", in_out);
+    // en.encrypt(&mut in_out);
+    // println!("a: {:?}", in_out);
+    // de.decrypt(&mut in_out);
+    // println!("b: {:?}", in_out);
+
+    let f = File::open("～/Downloads/ideaIU-2018.3.dmg").unwrap();
+    let mut fr = FileReader::new(&f, 0x6400000);
+
+    while let Some(m) = fr.next_mmap() {
+        println!("{:?}", m.len());
+    }
 }
